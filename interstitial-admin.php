a14d178a84b43631762d7157ddc9fd4d0babe774
Mike Levine <mike.levine@gmail.com>
2017-10-08 23:15:06 -0400

    adding defaults to all option vars

--- interstitial-admin.php
+++ interstitial-admin.php
@@ -34,18 +34,17 @@ function enqueue_date_picker(){
 			true
 		);
 		wp_enqueue_style( 'jquery-ui-datepicker' );
-}?>
+} 
 
-<?php function media_selector_settings_page_callback() {
-	wp_enqueue_media();
+function media_selector_settings_page_callback() {
 	// Save attachment ID
 	if ( isset( $_POST['submit_image_selector'] ) && isset( $_POST['image_attachment_id'] ) ) :
 		update_option( 'media_selector_attachment_id', absint( $_POST['image_attachment_id'] ) );
 	endif;
 
-	?>
-	<h3>Upload Background Image</h3>
-	<form method='post'>
+	wp_enqueue_media();
+
+	?><form method='post'>
 		<div class='image-preview-wrapper'>
 			<img id='image-preview' src='<?php echo wp_get_attachment_url( get_option( 'media_selector_attachment_id' ) ); ?>' height='100'>
 		</div>
@@ -53,9 +52,8 @@ function enqueue_date_picker(){
 		<input type='hidden' name='image_attachment_id' id='image_attachment_id' value='<?php echo get_option( 'media_selector_attachment_id' ); ?>'>
 		<input type="submit" name="submit_image_selector" value="Save" class="button-primary">
 	</form><?php
-} ?>
+} 
 
-<?php 
 function interstitial_init() { ?>
 
 <div class="wrap">
@@ -100,37 +98,37 @@ function interstitial_init() { ?>
 				<h3>Timing</h3>
                 <label>When would you like this ad to go live?</label>
                 <br />
-                <input type="date" id="datepicker" name="startdate-interstitial" value="<?php echo get_option('startdate-interstitial'); ?>" class="startdate-datepicker" />
+                <input type="date" id="datepicker" name="startdate-interstitial" value="<?php echo get_option('startdate-interstitial', $default = '1970-01-01'); ?>" class="startdate-datepicker" />
                 <br />
                 <label>When would you like this ad to end?</label>
                 <br />
-                <input type="date" id="datepicker_two" name="enddate-interstitial" value="<?php echo get_option('enddate-interstitial'); ?>" class="enddate-datepicker" />
+                <input type="date" id="datepicker_two" name="enddate-interstitial" value="<?php echo get_option('enddate-interstitial', $default = '2070-01-01'); ?>" class="enddate-datepicker" />
 				<br />
 				<label>How many times would you like to see this ad daily?</label>
                 <br />
                 <select name="daily" class="form-control" id="sel1">
-					<option value="1" <?php echo get_option('daily') == '1' ? 'selected' : '' ?>>1</option>
-					<option value="2" <?php echo get_option('daily') == '2' ? 'selected' : '' ?>>2</option>
+					<option value="1" <?php echo get_option('daily', $default = '1') == '1' ? 'selected' : '' ?>>1</option>
+					<option value="2" <?php echo get_option('daily', $default = '2') == '2' ? 'selected' : '' ?>>2</option>
 				</select>
 				<hr>
 				<h3>Dimensions</h3>
-				<input type="text" name="interstitial-width" value="<?php echo get_option('interstitial-width'); ?>" placeholder="width" />
-				<input type="text" name="interstitial-height" value="<?php echo get_option('interstitial-height'); ?>" placeholder="height" />
+				<input type="text" name="interstitial-width" value="<?php echo get_option('interstitial-width', $default = '500'); ?>" placeholder="width" />
+				<input type="text" name="interstitial-height" value="<?php echo get_option('interstitial-height', $default = '500'); ?>" placeholder="height" />
 				<hr>
 				<h3>DFP Code</h3>
 				<p>This is an example DFP code snippet. Please find the matching code for your ad, and enter only the code in <b>bold</b> into the DFP textfield below:</p>
-				<p><blockquote></blockquote><pre><code>&lt;div id='div-gpt-ad-<b>1502995302683</b>-0' style='height:600px; width:300px;'&gt;
+				<p><pre><code>&lt;div id='div-gpt-ad-<b>1502995302683</b>-0' style='height:600px; width:300px;'&gt;
 &lt;script&gt;
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1502995302683-0'); });
 &lt;/script&gt;
 &lt;/div&gt;
-</blockquote></code></pre>
-				<input type="text" name="interstitial-dfp-code" value="<?php echo get_option('interstitial-dfp-code'); ?>" placeholder="DFP Code" />
+</blockquote></code></pre></p>
+				<input type="text" name="interstitial-dfp-code" value="<?php echo get_option('interstitial-dfp-code', $default = 'sonicscoop'); ?>" placeholder="DFP Code" />
 				<?php wp_handle_upload( $file, $overrides, $time ); ?>
 				<br />
 				<hr>
 				<input type='hidden' value='0' name='activate_ad'>
-				<input class="activate_ad" style="float:left;position: relative;top:14px;" type="checkbox" name="activate_ad" value="<?php echo get_option('activate_ad'); ?>" <?php echo get_option('activate_ad') == '1' ? 'checked' : 'unchecked' ?> />
+				<input class="activate_ad" style="float:left;position: relative;top:14px;" type="checkbox" name="activate_ad" value="<?php echo get_option('activate_ad', $default = '0'); ?>" <?php echo get_option('activate_ad', $default = '0') == '1' ? 'checked' : 'unchecked' ?> />
             	<h3>Activate Ad?</h3>
 				<input type="submit" value="Save" class="button button-primary button-large">
         </div><!-- #options-container -->
@@ -181,4 +179,4 @@ function media_selector_print_scripts() {
 					file_frame.open();
 			});
 		});
-</script><?php } ?>
+</script><?php }
